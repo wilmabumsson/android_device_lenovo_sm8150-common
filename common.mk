LineageOS/android_device_oneplus_sm8150-common

sm8150-common: livedisplay: Uprev to 2.1
Change-Id: I98b2ea16ed342339ee652d5cb79dcfd22f8c0a0d

common.mk
@@ -304,7 +304,7 @@ PRODUCT_COPY_FILES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.oneplus_msmnile
+    vendor.lineage.livedisplay@2.1-service.oneplus_msmnile
 
 # Media
 PRODUCT_COPY_FILES += \

wilmabumsson/android_device_lenovo_sm8150-common

common.mk
@@ -273,7 +273,7 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.lenovo_msmnile
+    vendor.lineage.livedisplay@2.1-service.lenovo_msmnile
 
 # Media
 PRODUCT_PACKAGES += \