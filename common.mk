5c92cdce05653396de27248032c3fdd5f376fed1
Marc Bourgoin <themard85@gmail.com>
2021-05-13 09:28:38 -0600

    sm8150-common: Fix WFD
    We are missing some blobs so add them and include libavservices_minijail
    
    Change-Id: I8ef2d8986e29a93f44f91c3ba4620b7c0bb28b89

--- common.mk
+++ common.mk
@@ -277,6 +277,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    libavservices_minijail \
     libavservices_minijail_vendor \
     libavservices_minijail.vendor \
     libc2dcolorconvert \
